3f650328186dc734a75d7e8c297899ebead615ba
bubbi4232 <bubbi4232@gmail.com>
2024-10-27 15:18:07 -0400

    Update main.py
    Add min-math API route

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from typing import Optional
+#from typing import Optional
+#from fastapi import FastAPI
+from typing import Optional, List
+from fastapi import FastAPI, Query
 
-from fastapi import FastAPI
+def find_min(arr):
+  """ Returns the minimum value of an integer array."""
+  return min(arr)
 
 app = FastAPI()
 
@@ -12,3 +17,13 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/mini/")
+async def minMath(nums : List[int] = Query(None)):
+    s = min(nums)
+    print(s)
+    return s #{"smallest num": s   }
+#example: http://127.0.0.1:8000/mini/?nums=2&nums=7
+#returns 2
+
+ 
